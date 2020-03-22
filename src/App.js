018967327f59ad484e916aa2554f42347d67122c
Claire Liu <ccyliu7@gmail.com>
2020-03-22 14:01:02 -0700

    updated main page

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Claire Liu
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/claire-c-liu"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Connect with me on LinkedIn
         </a>
       </header>
     </div>
